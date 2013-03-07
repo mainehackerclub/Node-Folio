5c149f3b2a1e7ef11bc6fdd56aa97ae53221ff77
Garrett Wilkin <garrett.wilkin@gmail.com>
2013-03-06 19:59:44 -0500

    Adding winston logging & Civic Voicemail link.

--- hall.js
+++ hall.js
@@ -7,10 +7,13 @@ var static = require('node-static');
 var winston = require('winston');
 var file = new(static.Server)('.');
 
+// Winston logging setup.
 var logger = new winston.Logger;
 var  options = {timestamp:true};
 logger.add(winston.transports.Console, options);
-var HOST = 'localhost';
+var config = require('./config.json')['server'];
+logger.info(config.toString());
+var HOST = config.host;
 var port = 80;
 var server = http.createServer(function(req,res) {
   logger.info('HTTP request',req.method,req.url);
